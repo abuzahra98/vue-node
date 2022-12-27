78e3635a8015bfe8c28a59db24903ca8628f4307
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2022-12-27 14:50:49 +0300

    fix books

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,17 @@
   <HelloWorld />
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
 #app {
   display: grid;
-grid-template-columns: repeat(5, 1fr);
-gap: 20px;
-font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+    'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
   color: #88482d;
   margin-top: 60px;
 }
